f7315b736106d2f1351c5d44985157d62bfe4448
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-26 20:09:15 -0500

    Stream

---
+++ Streams1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private fileToLoad As New StreamReader("C:\myFile.txt")
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        TextBox1.Text = fileToLoad.ReadToEnd()
+    End Sub
+End Class
